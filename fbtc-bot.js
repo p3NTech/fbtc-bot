49840e18258f08423a5e963fd0b07486017f8075
Michał Wróblewski <debosgit@pm.me>
2019-05-08 20:38:38 +0100

    Add option to change default bet value

--- fbtc-bot.js
+++ fbtc-bot.js
@@ -1,11 +1,12 @@
 var CONFIG_SPEED=1
+var DEFAULT_BET=0.00000001
 
 function sleep(ms){return new Promise(resolve=>setTimeout(resolve,ms));}
 function bet_hi(){document.getElementById('double_your_btc_bet_hi_button').click()}
 function bet_lo(){document.getElementById('double_your_btc_bet_lo_button').click()}
 function is_win(){return document.getElementById('double_your_btc_bet_win').style.display=='block'}
 function get_code(){return document.getElementsByClassName('large-1 small-1 columns center lottery_winner_table_box lottery_winner_table_second_cell')[6].innerHTML}
-function reset_bet_amount(){document.getElementById('double_your_btc_min').click()}
+function reset_bet_amount(){document.getElementById('double_your_btc_stake').value=DEFAULT_BET}
 function double_bet_amount(){document.getElementById('double_your_btc_2x').click()}
 
 while(true){
