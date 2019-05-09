72e3a7b976e3d853f9b91e8d5ecc8d56f8061d20
Michał Wróblewski <debosgit@pm.me>
2019-05-09 07:11:38 +0100

    Add intelligent bet option

--- fbtc-bot.js
+++ fbtc-bot.js
@@ -1,12 +1,13 @@
 var CONFIG_SPEED=1
-var DEFAULT_BET=0.00000001
+var CONFIG_DEFAULT_BET=0.00000001
+var CONFIG_INTELLIGENT_BET=true
 
 function sleep(ms){return new Promise(resolve=>setTimeout(resolve,ms));}
 function bet_hi(){document.getElementById('double_your_btc_bet_hi_button').click()}
 function bet_lo(){document.getElementById('double_your_btc_bet_lo_button').click()}
 function is_win(){return document.getElementById('double_your_btc_bet_win').style.display=='block'}
 function get_code(){return document.getElementsByClassName('large-1 small-1 columns center lottery_winner_table_box lottery_winner_table_second_cell')[6].innerHTML}
-function reset_bet_amount(){document.getElementById('double_your_btc_stake').value=DEFAULT_BET}
+function reset_bet_amount(){document.getElementById('double_your_btc_stake').value=(CONFIG_INTELLIGENT_BET?document.getElementById('balance').innerHTML/1000:CONFIG_DEFAULT_BET)}
 function double_bet_amount(){document.getElementById('double_your_btc_2x').click()}
 
 while(true){
